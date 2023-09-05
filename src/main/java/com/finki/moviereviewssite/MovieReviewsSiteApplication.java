479ed94fd0e8f400d3e228ab68cd788ed7bbd9e4
miabosheva <boshevam@gmail.com>
2023-09-05 14:20:26 +0200

    Initial project setup

---
+++ src/main/java/com/finki/moviereviewssite/MovieReviewsSiteApplication.java
@@ -0,0 +1,13 @@
+package com.finki.moviereviewssite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewsSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReviewsSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finki/moviereviewssite/MovieReviewsSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finki.moviereviewssite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewsSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
